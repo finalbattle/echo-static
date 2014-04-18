77762eabfa8603de82bae492e938161378420056
zhangpeng <zhangpeng820213@gmail.com>
2014-04-18 17:01:25 +0800

    添加string说明

--- echo/views/index.py
+++ echo/views/index.py
@@ -5,7 +5,25 @@ from echo.shortcuts import *
 from echo.handlers import *
 
 @url("/", action="index")
-class Index(LoginHandler):
+class Index(Handler):
     def get(self):
         return self.render("%s.html" % self.kwargs["action"], action=self.kwargs["action"])
-
+@url("/basic/string", action="string")
+class Basic(Handler):
+    def get(self):
+        return self.render("basic/%s.html" % self.kwargs["action"], action=self.kwargs["action"])
+#@url("/dom/drags", action="drags")
+#@url("/fshow", action="fshow")
+#@url("/inputs", action="inputs")
+#@url("/forms", action="forms")
+#@url("/pages", action="pages")
+#@url("/collapse", action="collapse")
+#@url("/tables", action="tables")
+#@url("/navs", action="navs")
+#@url("/lists", action="lists")
+#@url("/panels", action="panels")
+#@url("/rows-cols", action="rows-cols")
+#@url("/grids", action="grids")
+#@url("/tooltips", action="tooltips")
+#@url("/alerts", action="alerts")
+#@url("/delegate", action="delegate")
